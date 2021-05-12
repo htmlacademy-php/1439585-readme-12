7857214484bf00799e3ca4f1e732229c6dd9245c
RedrikShukhart <redrikshu@gmail.com>
2021-05-12 10:46:51 +0300

    поправила форматирование
    поправила форматирование: отделила внутренности класса post_main, внутренности условий php

--- index.php
+++ index.php
@@ -98,55 +98,55 @@ $cards = [
                     </li>
                 </ul>
                 <?php if ($is_auth == 1): ?>
-                <!-- здесь должен быть PHP код, который показывает следующий тег по условию -->
-                <ul class="header__user-nav">
-                    <li class="header__profile">
-                        <a class="header__profile-link" href="#">
-                            <div class="header__avatar-wrapper">
-                                <img class="header__profile-avatar" src="img/userpic-medium.jpg" alt="Аватар профиля">
+                    <!-- здесь должен быть PHP код, который показывает следующий тег по условию -->
+                    <ul class="header__user-nav">
+                        <li class="header__profile">
+                            <a class="header__profile-link" href="#">
+                                <div class="header__avatar-wrapper">
+                                    <img class="header__profile-avatar" src="img/userpic-medium.jpg" alt="Аватар профиля">
+                                </div>
+                                <div class="header__profile-name">
+                                    <span>
+                                        <!--здесь должно быть имя пользователя-->
+                                    </span>
+                                    <svg class="header__link-arrow" width="10" height="6">
+                                        <use xlink:href="#icon-arrow-right-ad"></use>
+                                    </svg>
+                                </div>
+                            </a>
+                            <div class="header__tooltip-wrapper">
+                                <div class="header__profile-tooltip">
+                                    <ul class="header__profile-nav">
+                                        <li class="header__profile-nav-item">
+                                            <a class="header__profile-nav-link" href="#">
+                            <span class="header__profile-nav-text">
+                                Мой профиль
+                            </span>
+                                            </a>
+                                        </li>
+                                        <li class="header__profile-nav-item">
+                                            <a class="header__profile-nav-link" href="#">
+                            <span class="header__profile-nav-text">
+                                Сообщения
+                                <i class="header__profile-indicator">2</i>
+                            </span>
+                                            </a>
+                                        </li>
+                                        <li class="header__profile-nav-item">
+                                            <a class="header__profile-nav-link" href="#">
+                            <span class="header__profile-nav-text">
+                                Выход
+                            </span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
-                            <div class="header__profile-name">
-                                <span>
-                                    <!--здесь должно быть имя пользователя-->
-                                </span>
-                                <svg class="header__link-arrow" width="10" height="6">
-                                    <use xlink:href="#icon-arrow-right-ad"></use>
-                                </svg>
-                            </div>
-                        </a>
-                        <div class="header__tooltip-wrapper">
-                            <div class="header__profile-tooltip">
-                                <ul class="header__profile-nav">
-                                    <li class="header__profile-nav-item">
-                                        <a class="header__profile-nav-link" href="#">
-                          <span class="header__profile-nav-text">
-                            Мой профиль
-                          </span>
-                                        </a>
-                                    </li>
-                                    <li class="header__profile-nav-item">
-                                        <a class="header__profile-nav-link" href="#">
-                          <span class="header__profile-nav-text">
-                            Сообщения
-                            <i class="header__profile-indicator">2</i>
-                          </span>
-                                        </a>
-                                    </li>
-                                    <li class="header__profile-nav-item">
-                                        <a class="header__profile-nav-link" href="#">
-                          <span class="header__profile-nav-text">
-                            Выход
-                          </span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <a class="header__post-button button button--transparent" href="adding-post.html">Пост</a>
-                    </li>
-                </ul>
+                        </li>
+                        <li>
+                            <a class="header__post-button button button--transparent" href="adding-post.html">Пост</a>
+                        </li>
+                    </ul>
                 <?php endif; ?>
             </nav>
         </div>
@@ -240,90 +240,90 @@ $cards = [
             </div>
         </div>
         <div class="popular__posts"> 
-        <?php foreach ($cards as $card): ?>
-            <article class="popular__post post <?= $card['type'] ?>">
-                <header class="post__header">
-                    <h2><?= $card['title'] ?></h2>
-                </header>
-                <div class="post__main">
-                <?php if ($card['type'] == 'post-quote'): ?>
-                    <blockquote>
-                        <p>
-                            <?= $card['content'] ?>
-                        </p>
-                        <cite>Неизвестный Автор</cite>
-                    </blockquote>
-                <?php elseif ($card['type'] == 'post-link'): ?>
-                <div class="post-link__wrapper">
-                    <a class="post-link__external" href="http://<?= $card['content'] ?>" title="Перейти по ссылке"> 
-                        <div class="post-link__info-wrapper">
-                            <div class="post-link__icon-wrapper">
-                                <img src="https://www.google.com/s2/favicons?domain=<?=  $card['content'] ?>" alt="Иконка">
+            <?php foreach ($cards as $card): ?>
+                <article class="popular__post post <?= $card['type'] ?>">
+                    <header class="post__header">
+                        <h2><?= $card['title'] ?></h2>
+                    </header>
+                    <div class="post__main">
+                        <?php if ($card['type'] == 'post-quote'): ?>
+                            <blockquote>
+                                <p>
+                                    <?= $card['content'] ?>
+                                </p>
+                                <cite>Неизвестный Автор</cite>
+                            </blockquote>
+                        <?php elseif ($card['type'] == 'post-link'): ?>
+                            <div class="post-link__wrapper">
+                                <a class="post-link__external" href="http://<?= $card['content'] ?>" title="Перейти по ссылке"> 
+                                    <div class="post-link__info-wrapper">
+                                        <div class="post-link__icon-wrapper">
+                                            <img src="https://www.google.com/s2/favicons?domain=<?=  $card['content'] ?>" alt="Иконка">
+                                        </div>
+                                        <div class="post-link__info">
+                                            <h3><?= $card['title'] ?></h3>
+                                        </div>
+                                    </div>
+                                    <span><?= $card['content'] ?></span>
+                                </a>
                             </div>
-                            <div class="post-link__info">
-                                <h3><?= $card['title'] ?></h3>
+                        <?php elseif ($card['type'] == 'post-photo'): ?>
+                            <div class="post-photo__image-wrapper">
+                                <img src="img/<?= $card['content'] ?>" alt="Фото от пользователя" width="360" height="240">
                             </div>
-                        </div>
-                        <span><?= $card['content'] ?></span>
-                    </a>
-                </div>
-                <?php elseif ($card['type'] == 'post-photo'): ?>
-                <div class="post-photo__image-wrapper">
-                    <img src="img/<?= $card['content'] ?>" alt="Фото от пользователя" width="360" height="240">
-                </div>
-                <?php elseif ($card['type'] == 'post-video'): ?>
-                <div class="post-video__block">
-                    <div class="post-video__preview">
-                    <?= $card['content'] ?>
-                        <img src="img/coast-medium.jpg" alt="Превью к видео" width="360" height="188">
+                        <?php elseif ($card['type'] == 'post-video'): ?>
+                            <div class="post-video__block">
+                                <div class="post-video__preview">
+                                <?= $card['content'] ?>
+                                    <img src="img/coast-medium.jpg" alt="Превью к видео" width="360" height="188">
+                                </div>
+                                <a href="post-details.html" class="post-video__play-big button">
+                                    <svg class="post-video__play-big-icon" width="14" height="14">
+                                        <use xlink:href="#icon-video-play-big"></use>
+                                    </svg>
+                                    <span class="visually-hidden">Запустить проигрыватель</span>
+                                </a>
+                            </div>
+                        <?php elseif ($card['type'] == 'post-text'): ?>
+                            <p><?= $card['content'] ?></p>
+                        <?php endif; ?>
                     </div>
-                    <a href="post-details.html" class="post-video__play-big button">
-                        <svg class="post-video__play-big-icon" width="14" height="14">
-                            <use xlink:href="#icon-video-play-big"></use>
-                        </svg>
-                        <span class="visually-hidden">Запустить проигрыватель</span>
-                    </a>
-                </div>
-                <?php elseif ($card['type'] == 'post-text'): ?>
-                    <p><?= $card['content'] ?></p>
-                <?php endif; ?>
-                </div>
-                <footer class="post__footer">
-                    <div class="post__author">
-                        <a class="post__author-link" href="#" title="Автор">
-                            <div class="post__avatar-wrapper">
-                                <img class="post__author-avatar" src="img/<?= $card['avatar'] ?>" alt="Аватар пользователя">
-                            </div>
-                            <div class="post__info">
-                                <b class="post__author-name"><?= $card['user_name'] ?></b>
-                                <time class="post__time" datetime="">дата</time>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="post__indicators">
-                        <div class="post__buttons">
-                            <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
-                                <svg class="post__indicator-icon" width="20" height="17">
-                                    <use xlink:href="#icon-heart"></use>
-                                </svg>
-                                <svg class="post__indicator-icon post__indicator-icon--like-active" width="20" height="17">
-                                    <use xlink:href="#icon-heart-active"></use>
-                                </svg>
-                                <span>0</span>
-                                <span class="visually-hidden">количество лайков</span>
-                            </a>
-                            <a class="post__indicator post__indicator--comments button" href="#" title="Комментарии">
-                                <svg class="post__indicator-icon" width="19" height="17">
-                                    <use xlink:href="#icon-comment"></use>
-                                </svg>
-                                <span>0</span>
-                                <span class="visually-hidden">количество комментариев</span>
+                    <footer class="post__footer">
+                        <div class="post__author">
+                            <a class="post__author-link" href="#" title="Автор">
+                                <div class="post__avatar-wrapper">
+                                    <img class="post__author-avatar" src="img/<?= $card['avatar'] ?>" alt="Аватар пользователя">
+                                </div>
+                                <div class="post__info">
+                                    <b class="post__author-name"><?= $card['user_name'] ?></b>
+                                    <time class="post__time" datetime="">дата</time>
+                                </div>
                             </a>
                         </div>
-                    </div>
-                </footer>
-            </article>
-        <?php endforeach; ?> 
+                        <div class="post__indicators">
+                            <div class="post__buttons">
+                                <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
+                                    <svg class="post__indicator-icon" width="20" height="17">
+                                        <use xlink:href="#icon-heart"></use>
+                                    </svg>
+                                    <svg class="post__indicator-icon post__indicator-icon--like-active" width="20" height="17">
+                                        <use xlink:href="#icon-heart-active"></use>
+                                    </svg>
+                                    <span>0</span>
+                                    <span class="visually-hidden">количество лайков</span>
+                                </a>
+                                <a class="post__indicator post__indicator--comments button" href="#" title="Комментарии">
+                                    <svg class="post__indicator-icon" width="19" height="17">
+                                        <use xlink:href="#icon-comment"></use>
+                                    </svg>
+                                    <span>0</span>
+                                    <span class="visually-hidden">количество комментариев</span>
+                                </a>
+                            </div>
+                        </div>
+                    </footer>
+                </article>
+            <?php endforeach; ?> 
         </div>
     </div>
 </section>
